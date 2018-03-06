8f5b17470f5df04ccd753d954d7bef1c7ba22992
pjvalentini <pj.valentini@gmail.com>
2018-03-06 13:22:07 -0500

    adding to the slideshow

--- client/app/components/Home.jsx
+++ client/app/components/Home.jsx
@@ -66,38 +66,34 @@ class Home extends Component {
 	    return (
 	        <div>
 	        	{renderLinks()}
-				    <div className="text-center">
-		        	<h1>Welcome to AcuWare!</h1>
-		        	<h2>Please Sign up or Sign in</h2>
-		        </div>
             <div className="slider">
               <ul className="slides">
                 <li>
-                  <img src="https://lorempixel.com/580/250/nature/1"/>
+                  <img src="/images/lotus.png" style={{height: '400px'}}/>
                   <div className="caption center-align">
-                    <h3>This is our big Tagline!</h3>
-                    <h5 className="light grey-text text-lighten-3">Here's our small slogan.</h5>
+                    <h3>Welcome to AcuWare!</h3>
+                    <h5 className="light grey-text text-lighten-3">Your Acupuncture Study Resource.</h5>
                   </div>
                 </li>
                 <li>
-                  <img src="https://lorempixel.com/580/250/nature/2"/>
+                  <img src="/images/needles-dummy.jpg"/>
                   <div className="caption left-align">
-                    <h3>Left Aligned Caption</h3>
-                    <h5 className="light grey-text text-lighten-3">Here's our small slogan.</h5>
+                    <h3>Sign Up Today!</h3>
+                    <h5 className="light grey-text text-lighten-3">Start Your Learning Experience.</h5>
                   </div>
                 </li>
                 <li>
-                  <img src="https://lorempixel.com/580/250/nature/3"/>
+                  <img src="/images/needles.png"/>
                   <div className="caption right-align">
-                    <h3>Right Aligned Caption</h3>
-                    <h5 className="light grey-text text-lighten-3">Here's our small slogan.</h5>
+                    <h3>Create Index Cards More Effenciently.</h3>
+                    <h5 className="light grey-text text-lighten-3">Our data is just what you need.</h5>
                   </div>
                 </li>
                 <li>
-                  <img src="https://lorempixel.com/580/250/nature/4"/>
+                  <img src="/images/anciettcmtext.jpg"/>
                   <div className="caption center-align">
-                    <h3>This is our big Tagline!</h3>
-                    <h5 className="light grey-text text-lighten-3">Here's our small slogan.</h5>
+                    <h3>Always Free!</h3>
+                    <h5 className="light grey-text text-lighten-3">All you have to is sign up ans start learning.</h5>
                   </div>
                 </li>
               </ul>
